7a9b37499b4e366b6b75ec93bdacf447964e5913
mahnoorahsen <156011562+mahnoorahsen@users.noreply.github.com>
2024-02-15 19:24:06 -0500

    Update ComputeReadingLevel.java
    Adjusted spacing

--- ComputeReadingLevel.java
+++ ComputeReadingLevel.java
@@ -11,59 +11,57 @@ import java.util.Scanner;
 
 public class ComputeReadingLevel 
 {
-
 	public static void main(String[] args) 
 	{
-			Scanner input = new Scanner(System.in);
+	   Scanner input = new Scanner(System.in);
 			
-			String userText = "";
+	   String userText = "";
 			
-			//Prompt user to enter text.
-			System.out.println("Enter some text to determine the approximate grade level needed for comprehension (use proper end punctuation).");
-			System.out.print("Text: ");
-			userText = input.nextLine().trim();
+	   //Prompt user to enter text.
+	   System.out.println("Enter some text to determine the approximate grade level needed for comprehension (use proper end punctuation).");
+	   System.out.print("Text: ");
+           userText = input.nextLine().trim();
 	
-			//Handle input errors
-			//Ensure text contains at least one sentence, and allow user to enter text again.
-			while(userText.matches("\\s+|\\s|[.?!]") || userText.isEmpty() || userText.matches("\\d+") || (!userText.matches(".*[.?!]")))	
-			{
-				System.out.println("Invalid input. Enter at least one complete sentence to determine the approximate grade level needed for comprehension: ");
-				userText = input.nextLine().trim();
-			}
+	   //Handle input errors
+	   //Ensure text contains at least one sentence, and allow user to enter text again.
+	   while(userText.matches("\\s+|\\s|[.?!]") || userText.isEmpty() || userText.matches("\\d+") || (!userText.matches(".*[.?!]")))	
+	   {
+	      System.out.println("Invalid input. Enter at least one complete sentence to determine the approximate grade level needed for comprehension: ");
+	      userText = input.nextLine().trim();
+	   }
+	   //Count letters including alphabets only.
+	   int userLetters = userText.replaceAll("[^a-zA-Z]","").length();
 			
-			//Count letters including alphabets only.
-			int userLetters = userText.replaceAll("[^a-zA-Z]","").length();
+	   //Count words by splitting at spaces, exclude punctuation.
+	   int userWords = userText.replaceAll("[^a-zA-z\\s+]", "").trim().split("\\s").length;  
 			
-			//Count words by splitting at spaces, exclude punctuation.
-			int userWords = userText.replaceAll("[^a-zA-z\\s+]", "").trim().split("\\s").length;  
-			
-			//Count sentences by splitting text after any end punctuation. 
-			int userSentences = userText.split("[!.?]").length;      
+	   //Count sentences by splitting text after any end punctuation. 
+	   int userSentences = userText.split("[!.?]").length;      
 	
-			//Use getReadingLevel method to display result.
-			getReadingLevel(userLetters, userSentences, userWords);
+	   //Use getReadingLevel method to display result.
+	   getReadingLevel(userLetters, userSentences, userWords);
 			
-			input.close();
+	   input.close();
 	}
 	
 	//Method to compute Coleman-Liau index and display grade level needed for comprehension of text.	
 	public static void getReadingLevel(int userLetters, int userSentences, int userWords)
 	{
-			//Calculate average letters per 100 words.
-			double L = ((double) userLetters/userWords)*100;  
+	   //Calculate average letters per 100 words.
+	   double L = ((double) userLetters/userWords)*100;  
 			
-			//Calculate average sentences per 100 words.
-			double S = ((double) userSentences/userWords)*100;   
+	   //Calculate average sentences per 100 words.
+	   double S = ((double) userSentences/userWords)*100;   
 			
-			//Coleman-Liau formula to calculate index.
-			int gradeLevel = (int) (0.0588 * L -0.296 * S - 15.8);  
+	  //Coleman-Liau formula to calculate index.
+	   int gradeLevel = (int) (0.0588 * L -0.296 * S - 15.8);  
 			
-			//Set index value to be in the proper grade range. (Grades 1-12)
-			gradeLevel = Math.max(gradeLevel, 1); 
-			gradeLevel = Math.min(gradeLevel, 12);
+	  //Set index value to be in the proper grade range. (Grades 1-12)
+	  gradeLevel = Math.max(gradeLevel, 1); 
+	  gradeLevel = Math.min(gradeLevel, 12);
 			
-			//Display calculated grade level.
-			System.out.print("\nReadability Result: Grade " + gradeLevel);
+	  //Display calculated grade level.
+          System.out.print("\nReadability Result: Grade " + gradeLevel);
 	}
 	
 
